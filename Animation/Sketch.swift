45ec536761645bc6386fd5e28ac7d293b13cc5d3
rsgc-lo-a <alo@rsgc.on.ca>
2016-01-14 15:39:15 -0500

    Direction of the ball
    Coming from 4 different different, will add the code when the ball hits
    the end of the canvas, it will go back to its side with different
    direction if possible. All 4 different ball will come from all for sides

--- Animation/Sketch.swift
+++ Animation/Sketch.swift
@@ -7,10 +7,18 @@ class Sketch {
     let canvas : Canvas
     
     //Random number generator with a variable (repeated)
-    var random = Int(arc4random_uniform(500) - 30)
+
+    var random = Int(arc4random_uniform(500))
+  
     //variable that is going to increase
     var x = 0
     var y = 0
+    
+    //Variable that is going to decrease
+    var negx = 0
+    var negy = 0
+    var maxX = 500
+    var maxY = 500
     
     // This runs once, equivalent to setup() in Processing
     init() {
@@ -20,18 +28,24 @@ class Sketch {
         
         // The frame rate can be adjusted; the default is 60 fps
         canvas.framesPerSecond = 60
-    
      }
     
     // Runs repeatedly, equivalent to draw() in Processing
     func draw() {
-        
+    
         
         //Self increasing variables
         x = x + 5
         y = y + 5
+        
+        negx = negx - 5
+        negy = negy - 5
+        maxX = maxX - 5
+        maxY = maxY - 5
+        
         // Clear the background
         //Create a rectangle with the given canvas size
+        
         canvas.drawShapesWithBorders = false
         canvas.fillColor = Color(hue: 0, saturation: 0, brightness: 0, alpha: 100)
         canvas.drawRectangle(bottomRightX: 0, bottomRightY: 0, width: canvas.width, height: canvas.height)
@@ -41,12 +55,22 @@ class Sketch {
         canvas.fillColor = Color(hue: 100, saturation: 100, brightness: 100, alpha: 100)
         canvas.drawRectangle(bottomRightX: canvas.width / 2, bottomRightY: canvas.height / 2, width: 25, height: 25)
         
-        
-        //New colour planned for the circle
-        
-        //Draw a circle to represent, random location if possible
-        canvas.drawEllipse(centreX:random + x, centreY:random + y, width: 30, height: 30)
-        
+        //Going right from the left
+        //Draw a circle to represent, random location
+        //canvas.drawEllipse(centreX: x, centreY:random, width: 30, height: 30)
+       
+        //Going upward from the botto
+        //Draw a circle to represent, random location
+     //canvas.drawEllipse(centreX:random, centreY:y, width: 30, height: 30)
+//        
+//        Going left from the right
+//        //Draw a circle to represent, random location if
+        canvas.drawEllipse(centreX: maxX , centreY: random, width: 30, height: 30)
+//
+//        
+//        //Draw a circle to represent, random location
+//        canvas.drawEllipse(centreX:random4 + x, centreY:random4 + y, width: 30, height: 30)
+//
     }
     
 }
